c63990cb43e640cbf55b21a9cd6c2e954bd548e7
imtiazhasanBD <imtiazhasan500@gmail.com>
2025-02-07 16:47:14 +0600

    add routing and layout components for users and products

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Users from './pages/Users';
+import Products from './pages/Products';
+import Layout from './components/Layout';
 
 const App = () => {
   return (
-    <div>
-       <h1>Welcome to Admin Dashboard</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout><h1>Welcome to Admin Dashboard</h1></Layout>} />
+        <Route path="/users" element={<Layout><Users /></Layout>} />
+        <Route path="/products" element={<Layout><Products /></Layout>} />
+      </Routes>
+    </Router>
   );
 };
 
---
+++ src/components/Layout.jsx
@@ -0,0 +1,13 @@
+import Sidebar from "./Sidebar";
+
+
+const Layout = ({ children }) => {
+  return (
+    <div className="flex">
+      <Sidebar />
+      <main className="flex-1 p-6">{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
